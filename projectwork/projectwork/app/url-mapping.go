8a3ce32eac1c7251bfcc5cf9721ceaabfabd43e2
archi6830 <92037087+archi6830@users.noreply.github.com>
2021-11-10 14:36:58 +0200

    Add files via upload

---
+++ projectwork/projectwork/app/url-mapping.go
@@ -0,0 +1,10 @@
+package app
+import (
+    "github.com/archi6830/workgo/projectwork/controller/ping"
+)
+func mapUrls(){
+    router.GET("/ping", ping.Ping )
+
+    router.GET("/Tweets/:Tweet_id", Tweets.GetAllTweets)
+    router.POST("/Tweets", Tweets.CreateTweets)
+}
---
+++ projectwork/projectwork/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+        "github.com/archi6830/wrkgo/projectwork/app"
+)
+func main() {
+        app.StartApplication()
+}
